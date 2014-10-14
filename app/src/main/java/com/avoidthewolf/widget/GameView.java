e10ddb16e52b4ae4918b62b248d67f2ddaa83902
Martin <martin.dieny@gmail.com>
2014-10-14 16:00:02 +0200

    code source de base

---
+++ app/src/main/java/com/avoidthewolf/widget/GameView.java
@@ -0,0 +1,21 @@
+package com.avoidthewolf.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
